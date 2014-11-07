b6b696df92d95b91736f16da8bbfe4e0d3bbb611
Joe Levin <jlevin@apppartner.com>
2014-11-07 15:07:20 -0500

    Changed operation to take in a file of words as an argument and output a random item

--- RandomLetter/main.m
+++ RandomLetter/main.m
@@ -12,15 +12,25 @@ int main(int argc, const char *argv[])
 {
     @autoreleasepool
     {
-        NSMutableArray *letters = [NSMutableArray array];
-        for (char letter = 'a'; letter <= 'z'; letter++)
-        {
-            [letters addObject:[NSString stringWithFormat:@"%c", letter]];
-        }
-        NSString *randomLetter = letters[arc4random_uniform([letters count])];
+        NSURL *fileURL = [NSURL fileURLWithPath:[NSString stringWithUTF8String:argv[1]]];
+        NSError *fileReadError;
+        NSString *file = [NSString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:&fileReadError];
+        if (fileReadError)
+            return 0;
+
+        NSArray *delimitedWords = [file componentsSeparatedByString:@"\n"];
+        NSString *randomWord = delimitedWords[arc4random_uniform([delimitedWords count])];
+
+        //        NSMutableArray *letters = [NSMutableArray array];
+        //        for (char letter = 'a'; letter <= 'z'; letter++)
+        //        {
+        //            [letters addObject:[NSString stringWithFormat:@"%c", letter]];
+        //        }
+        //        NSString *randomLetter = letters[arc4random_uniform([letters count])];
         NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
         [dateFormatter setDateStyle:NSDateFormatterShortStyle];
-        NSLog(@"The random letter for %@ is %@", [dateFormatter stringFromDate:[NSDate date]], randomLetter);
+        //        NSLog(@"The random letter for %@ is %@", [dateFormatter stringFromDate:[NSDate date]], randomLetter);
+        NSLog(@"The random word for %@ is %@", [dateFormatter stringFromDate:[NSDate date]], randomWord);
     }
     return 0;
 }
